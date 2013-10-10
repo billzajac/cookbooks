9d28bfb0c266483aa0a86ff90917409bcced9c03
William Zajac <william.zajac@disney.com>
2013-10-10 15:51:03 -0700

    updated versions

--- statsd/metadata.rb
+++ statsd/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "git@billzajac.com"
 license          "Apache 2.0"
 description      "Installs/Configures statsd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.6.0-1"
+version          "0.6.0"
 
 depends "build-essential"
 depends "git"
